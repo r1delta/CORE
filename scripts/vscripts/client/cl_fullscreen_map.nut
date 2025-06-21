r1delta/CORE

commit 5cefb7360d93919fba13fcfd20e80d932a55f379

Fix FD minimap scaling on certain maps (#44)

scripts/vscripts/client/levels/cl_mp_mia.nut
@@ -1,0 +1,11 @@
+
+function main()
+{
+	if ( GAMETYPE == COOPERATIVE )
+		SetCustomMinimapZoom( 2 )
+}
+
+function EntitiesDidLoad()
+{
+
+}

scripts/vscripts/client/levels/cl_mp_runoff.nut
@@ -1,0 +1,11 @@
+
+function main()
+{
+	if ( GAMETYPE == COOPERATIVE )
+		SetCustomMinimapZoom( 2 )
+}
+
+function EntitiesDidLoad()
+{
+
+}

scripts/vscripts/client/levels/cl_mp_swampland.nut
@@ -1,0 +1,11 @@
+
+function main()
+{
+	if ( GAMETYPE == COOPERATIVE )
+		SetCustomMinimapZoom( 2 )
+}
+
+function EntitiesDidLoad()
+{
+
+}

scripts/vscripts/client/levels/cl_mp_wargames.nut
@@ -1,0 +1,11 @@
+
+function main()
+{
+	if ( GAMETYPE == COOPERATIVE )
+		SetCustomMinimapZoom( 2 )
+}
+
+function EntitiesDidLoad()
+{
+
+}

scripts/vscripts/client/levels/cl_mp_nest2.nut
@@ -9,6 +9,8 @@ function main()
 	level.musicEnabled = true //Turn off all music in this level
 	
 	SetFullscreenMinimapParameters( 3, 0, 0, 90 )
+	if ( GAMETYPE == COOPERATIVE )
+		SetCustomMinimapZoom( 2 )
 
 	IncludeFileAllowMultipleLoads( "client/objects/cl_ai_turret" )
 	// IncludeFileAllowMultipleLoads( "client/objects/cl_ai_turret_bb" )

commit 5181408863fd5981159129ffc3d8b82693fc0cbc

Fix fullscreen minimap zoom for a few maps
Fixes the fullscreen minimap being broken after https://github.com/r1delta/CORE/commit/5cefb7360d93919fba13fcfd20e80d932a55f379

Looks like the game auto-generates it somehow, when there isn't a `cl_MAPNAME.nut` file. But when there *is*, it tries to get custom override parameters even if they don't exist

scripts/vscripts/client/cl_fullscreen_map.nut
@@ -130,3 +130,12 @@ function SetFullscreenMinimapParameters( zoomAmount, xPos, yPos, rotation )
 	file.fullscreenMinimapRotation = rotation
 }
 Globalize( SetFullscreenMinimapParameters )
+
+function PrintFullscreenMinimapParameters()
+{
+	printt( file.fullscreenMinimapZoomAmount )
+	printt( file.fullscreenMinimapXPos )
+	printt( file.fullscreenMinimapYPos )
+	printt( file.fullscreenMinimapRotation )
+}
+Globalize( PrintFullscreenMinimapParameters )

scripts/vscripts/client/levels/cl_mp_mia.nut
@@ -1,6 +1,7 @@
 
 function main()
 {
+	SetFullscreenMinimapParameters( 3.5, 500, 3500, -90 )
 	if ( GAMETYPE == COOPERATIVE )
 		SetCustomMinimapZoom( 2 )
 }

scripts/vscripts/client/levels/cl_mp_runoff.nut
@@ -1,6 +1,7 @@
 
 function main()
 {
+	SetFullscreenMinimapParameters( 5.1, 600, -950, 180 )
 	if ( GAMETYPE == COOPERATIVE )
 		SetCustomMinimapZoom( 2 )
 }

scripts/vscripts/client/levels/cl_mp_swampland.nut
@@ -1,6 +1,7 @@
 
 function main()
 {
+	SetFullscreenMinimapParameters( 4, -3000, -200, 180 )
 	if ( GAMETYPE == COOPERATIVE )
 		SetCustomMinimapZoom( 2 )
 }

scripts/vscripts/client/levels/cl_mp_wargames.nut
@@ -1,6 +1,7 @@
 
 function main()
 {
+	SetFullscreenMinimapParameters( 4.9, -1000, 250, 180 )
 	if ( GAMETYPE == COOPERATIVE )
 		SetCustomMinimapZoom( 2 )
 }